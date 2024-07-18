14c1da54fa8d4da0bc68e7561d17d0a85330c839
Saad <alsuabies@gmail.com>
2024-07-18 14:26:59 +0100

    Handle command line arguments

--- cli/main.cpp
+++ cli/main.cpp
@@ -1,10 +1,47 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 int main(int argc,const char* argv[])
 {
-  cout << "hello world!" << endl;
-  return 0;
+    if(argc == 2)  //./program filename
+    {
+
+    }
+    else if(argc == 3) // ./program filename word_to_find
+    {
+
+    }
+    else if(argc == 4) // ./program filename word_to_find -regex
+    {
+        const char* filename;
+        const char* to_find;
+        if(strcmp(argv[1],"-regex") == 0)
+        {
+            filename = argv[2];
+            to_find = argv[3];
+        }
+        else if(strcmp(argv[2],"-regex") == 0)
+        {
+            filename = argv[1];
+            to_find = argv[3];
+        }
+        else if(strcmp(argv[3],"-regex") == 0)
+        {
+            filename = argv[1];
+            to_find = argv[2];
+        }
+        else
+        {
+            cerr << "invalid usage "<< endl;
+            return 1;
+        }
+    }
+    else
+    {
+        cerr << "invalid usage" << endl;
+        return 1;
+    }
 }
 

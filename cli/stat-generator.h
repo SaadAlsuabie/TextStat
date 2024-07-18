404d608f505a7a10ddd3728d516ec445a994e8c3
Saad <alsuabies@gmail.com>
2024-07-18 14:29:42 +0100

    Add stat-generator class

---
+++ cli/stat-generator.h
@@ -0,0 +1,13 @@
+#ifndef STAT_GENERATOR_H_
+#define STAT_GENERATOR_H_
+
+#include <string>
+/* Class to implement all the juicy functionality related to text stats*/
+class stat_generator
+{
+private:
+    std::string filename;
+    std::string source;
+
+};
+#endif

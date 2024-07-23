b00df0933eb2bf2b354b04e7a54a4d2a58400312
Saad <alsuabies@gmail.com>
2024-07-23 18:04:47 +0100

    Add class to represent word

--- cli/stat-generator.h
+++ cli/stat-generator.h
@@ -2,6 +2,22 @@
 #define STAT_GENERATOR_H_
 
 #include <string>
+
+/* Class to represent a word along with it's source i.e linenumber */
+class word
+{
+private:
+    std::string value;
+    size_t line_num;
+    size_t word_num;
+public:
+    word();
+    word(const std::string& val,size_t ln,size_t word_num);
+    const std::string& get_value();
+    size_t get_line_num();
+    size_t get_word_num();
+};
+
 /* Class to implement all the juicy functionality related to text stats*/
 class stat_generator
 {
